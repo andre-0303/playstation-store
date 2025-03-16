25e55ed7d22679a8407920596504e3b0d2db7a32
andre-0303 <bandeiradev.ti@gmail.com>
2025-03-16 12:32:07 -0300

    Projeto Playstation Store

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const menuList = document.getElementById("menu-list");
+
+menuList.style.maxHeight = "0px";
+
+function showMenu() {
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "120px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+
+function add() {
+    alert("Adicionado ao carrinho!")
+}
